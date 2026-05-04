5470702118332a7cace6df4dcd9d539a1c171403
MarcoRivera06 <marco.rivera@epn.edu.ec>
2026-05-03 23:57:27 -0500

    Completando

--- src/AlexanderPaca/SoloCarpetaNoVacia.java
+++
@@ -1,5 +0,0 @@
-package AlexanderPaca;
-
-public class SoloCarpetaNoVacia {
-
-}
--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
+import poliRetosAlexander.controladorAlexander;
 import PoliRetosMarco.ControladorPoliRetosMarco;
 
 public class App {
     public static void main(String[] args) throws Exception {
         ControladorPoliRetosMarco controlador = new ControladorPoliRetosMarco();
         controlador.showSerieNumerica();
+
+        controladorAlexander controladorAlexander = new controladorAlexander();
+        controladorAlexander.showSeries();
+
     }
 }
